bc6f5d758d9d6d81c46e9096b6bc263a7ffd9387
Chris Malloy <cjmalloy@gmail.com>
2014-11-12 21:11:32 -0400

    Set workspace persistence and defaults.

--- src/main/java/com/cjmalloy/torrentfs/editor/controller/FileSystemController.java
+++ src/main/java/com/cjmalloy/torrentfs/editor/controller/FileSystemController.java
@@ -1,20 +1,26 @@
 package com.cjmalloy.torrentfs.editor.controller;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.prefs.Preferences;
 
 import com.cjmalloy.torrentfs.editor.model.FileSystemModel;
 
 
 public class FileSystemController extends Controller<FileSystemModel>
 {
+    private static Preferences P = Preferences.userNodeForPackage(FileSystemController.class);
 
     public FileSystemController(FileSystemModel model)
     {
         this.model = model;
+
+        setWorkspace(Paths.get(P.get("tfsLastWorkspace", System.getProperty("user.home"))));
     }
 
     public void setWorkspace(Path workspace)
     {
+        P.put("tfsLastWorkspace", workspace.toString());
         model.workspace = workspace;
         update();
     }
