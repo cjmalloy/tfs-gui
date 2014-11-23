ca49fc5630683a1da6c180ecd299e7c2fffb4d39
Chris Malloy <cjmalloy@gmail.com>
2014-11-23 18:11:20 -0400

    Fixed workspace tree model change events.

--- src/main/java/com/cjmalloy/torrentfs/editor/ui/swing/model/FileTreeModel.java
+++ src/main/java/com/cjmalloy/torrentfs/editor/ui/swing/model/FileTreeModel.java
@@ -1,8 +1,9 @@
 package com.cjmalloy.torrentfs.editor.ui.swing.model;
 
 import java.io.File;
-import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import javax.swing.event.TreeModelEvent;
@@ -33,29 +34,41 @@ public class FileTreeModel implements TreeModel
     }
 
     @Override
-    public Object getRoot()
+    public void addTreeModelListener(TreeModelListener listener)
     {
-        return root;
+        listeners.add(listener);
     }
 
     @Subscribe
     public void fileModified(FileModificationEvent event)
     {
-        // TODO: this does not appear to work
-        fireTreeNodesChanged(getPathForFile(event.file), null, null);
-    }
-
-    private TreePath getPathForFile(Path file)
-    {
-        return new TreePath(file);
+        System.out.println(event.file);
+        try
+        {
+            switch (event.type)
+            {
+            case CREATE:
+                fireTreeNodesInserted(event.file.toFile());
+                break;
+            case DELETE:
+                fireTreeNodesDeleted(event.file.toFile());
+                break;
+            case MODIFY:
+                fireTreeNodesChanged(event.file.toFile());
+                break;
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Object getChild(Object parent, int index)
     {
         File directory = (File) parent;
-        String[] children = directory.list();
-        return new TreeFile(directory, children[index]);
+        return listDirectory(directory)[index];
     }
 
     @Override
@@ -65,9 +78,28 @@ public class FileTreeModel implements TreeModel
         if (file.isDirectory())
         {
             String[] fileList = file.list();
-            if (fileList != null) return file.list().length;
+            if (fileList != null) return fileList.length;
         }
         return 0;
+    }
+
+    @Override
+    public int getIndexOfChild(Object parent, Object child)
+    {
+        File directory = (File) parent;
+        File file = (File) child;
+        TreeFile[] children = listDirectory(directory);
+        for (int i = 0; i < children.length; i++)
+        {
+            if (file.equals(children[i])) { return i; }
+        }
+        return -1;
+    }
+
+    @Override
+    public Object getRoot()
+    {
+        return root;
     }
 
     @Override
@@ -77,18 +109,22 @@ public class FileTreeModel implements TreeModel
         return file.isFile();
     }
 
-    @Override
-    public int getIndexOfChild(Object parent, Object child)
+    public TreeFile[] listDirectory(File dir)
     {
-        File directory = (File) parent;
-        File file = (File) child;
-        String[] children = directory.list();
-        for (int i = 0; i < children.length; i++)
+        File[] children = dir.listFiles();
+        TreeFile[] sorted = new TreeFile[children.length];
+        for (int i=0; i<children.length; i++)
         {
-            if (file.getName().equals(children[i])) { return i; }
+            sorted[i] = new TreeFile(children[i]);
         }
-        return -1;
+        Arrays.sort(sorted);
+        return sorted;
+    }
 
+    @Override
+    public void removeTreeModelListener(TreeModelListener listener)
+    {
+        listeners.remove(listener);
     }
 
     @Override
@@ -103,7 +139,20 @@ public class FileTreeModel implements TreeModel
         int[] changedChildrenIndices = { getIndexOfChild(parent, targetFile) };
         Object[] changedChildren = { targetFile };
         fireTreeNodesChanged(path.getParentPath(), changedChildrenIndices, changedChildren);
+    }
 
+    private void fireTreeNodesChanged(File file)
+    {
+        fireTreeNodesChanged(getPathForFile(file));
+    }
+
+    private void fireTreeNodesChanged(TreePath parentPath)
+    {
+        TreeModelEvent event = new TreeModelEvent(this, parentPath);
+        for (TreeModelListener l : listeners)
+        {
+            l.treeNodesChanged(event);
+        }
     }
 
     private void fireTreeNodesChanged(TreePath parentPath, int[] indices, Object[] children)
@@ -115,25 +164,78 @@ public class FileTreeModel implements TreeModel
         }
     }
 
-    @Override
-    public void addTreeModelListener(TreeModelListener listener)
+    private void fireTreeNodesDeleted(File file)
     {
-        listeners.add(listener);
+        File parentFile = file.getParentFile();
+        TreePath parentPath = getPathForFile(parentFile);
+        int[] indices = {getDeletedIndex(parentFile, file)};
+        Object[] children = {new TreeFile(file)};
+        TreeModelEvent event = new TreeModelEvent(this, parentPath, indices, children);
+        for (TreeModelListener l : listeners)
+        {
+            l.treeNodesRemoved(event);
+        }
     }
 
-    @Override
-    public void removeTreeModelListener(TreeModelListener listener)
+    private void fireTreeNodesInserted(File file)
     {
-        listeners.remove(listener);
+        File parentFile = file.getParentFile();
+        TreePath parentPath = getPathForFile(parentFile);
+        int[] indices = {getIndexOfChild(parentFile, file)};
+        Object[] children = {new TreeFile(file)};
+        TreeModelEvent event = new TreeModelEvent(this, parentPath, indices, children);
+        for (TreeModelListener l : listeners)
+        {
+            l.treeNodesInserted(event);
+        }
+    }
+
+    private int getDeletedIndex(File directory, File file)
+    {
+        List<String> children = new ArrayList<>(Arrays.asList(directory.list()));
+        children.add(file.getName());
+        Collections.sort(children);
+        for (int i = 0; i < children.size(); i++)
+        {
+            if (file.getName().equals(children.get(i))) { return i; }
+        }
+        return -1;
+    }
+
+    private TreePath getPathForFile(File file)
+    {
+        List<File> path = new ArrayList<>();
+        while (!file.equals(root))
+        {
+            path.add(file);
+            file = file.getParentFile();
+        }
+        path.add(root);
+        Collections.reverse(path);
+        return new TreePath(path.toArray());
     }
 
     private class TreeFile extends File
     {
         private static final long serialVersionUID = 1L;
 
+        public TreeFile(File f)
+        {
+            super(f.toString());
+        }
+
         public TreeFile(File parent, String child)
         {
             super(parent, child);
+        }
+
+        @Override
+        public int compareTo(File pathname)
+        {
+            if (getName().endsWith(".tfs")) return -1;
+            if (pathname.getName().endsWith(".tfs")) return 1;
+            if (isDirectory() == pathname.isDirectory()) return super.compareTo(pathname);
+            return isDirectory() ? -1 : 1;
         }
 
         public String toString()
